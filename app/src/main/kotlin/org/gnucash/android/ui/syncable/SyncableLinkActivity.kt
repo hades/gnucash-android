c8997b36e6ca8f034663dcae9f72a4cc6426611b
Edward Toroshchyn <dev@hades.name>
2024-06-21 11:55:35 +0200

    wip: syncable

--- app/src/main/kotlin/org/gnucash/android/ui/syncable/SyncableLinkActivity.kt
+++ app/src/main/kotlin/org/gnucash/android/ui/syncable/SyncableLinkActivity.kt
@@ -1,20 +1,94 @@
 package org.gnucash.android.ui.syncable
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
 
-import org.gnucash.android.R
+private class CodeScannerHolder {
+    var scanner: CodeScanner? = null
+}
 
-class SyncableLinkActivity : AppCompatActivity() {
+class SyncableLinkActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_syncable_link)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, SyncableLinkFragment.newInstance())
-                .commitNow()
+        val viewModel: SyncableLinkViewModel by viewModels()
+        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            viewModel.cameraPermissionRequestComplete(isGranted)
+        }
+        ensureCameraPermissions(requestPermissionLauncher)
+        setContent {
+            val state = viewModel.uiState.collectAsState()
+            Scaffold(
+                topBar = { TopAppBar(title = { Text("Pairing") }) }
+            ) { innerPadding ->
+                Column(modifier = Modifier.padding(innerPadding)) {
+                    when (val stage = state.value.stage) {
+                        SyncableLinkStage.WaitingForCameraPermission -> WaitingForCameraUi()
+                        SyncableLinkStage.Scanning -> ScanningUi { viewModel.qrCodeScanned(it) }
+                        is SyncableLinkStage.Confirming -> ConfirmingUi()
+                        is SyncableLinkStage.Failed -> ErrorUi(stage.message)
+                    }
+                }
+            }
         }
     }
 
+    private fun ensureCameraPermissions(requestPermissionLauncher: ActivityResultLauncher<String>) {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> return
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+}
+
+@Composable
+fun WaitingForCameraUi() {
+    Text("waiting for camera")
+}
+
+@Composable
+fun ScanningUi(scannedCallback: (String) -> Unit) {
+    val scannerHolder = remember { CodeScannerHolder() }
+    AndroidView(
+        factory = { context ->
+            CodeScannerView(context).apply {
+                scannerHolder.scanner = CodeScanner(context, this).apply {
+                    decodeCallback = DecodeCallback { scannedCallback(it.text) }
+                    startPreview()
+                }
+            }
+        },
+    )
+}
+
+@Composable
+fun ConfirmingUi() {
+    Text ( "confirming" )
+}
+
+@Composable
+fun ErrorUi(msg: String) {
+   Text ( "error: $msg")
 }
