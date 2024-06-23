954e08c501f96c5cf5d299852a0eb7813996d13b
Edward Toroshchyn <dev@hades.name>
2024-06-23 10:11:17 +0200

    wip syncable

--- app/src/main/kotlin/org/gnucash/android/ui/syncable/SyncableLinkViewModel.kt
+++ app/src/main/kotlin/org/gnucash/android/ui/syncable/SyncableLinkViewModel.kt
@@ -1,7 +1,11 @@
 package org.gnucash.android.ui.syncable
 
+import android.util.Base64
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.fzakaria.ascii85.Ascii85
+import com.google.crypto.tink.BinaryKeysetReader
+import com.google.crypto.tink.CleartextKeysetHandle
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -32,7 +36,15 @@ class SyncableLinkViewModel() : ViewModel() {
         }
     }
 
-    fun qrCodeScanned(data: String) {
-        Timber.d("scanned message: %s", data)
+    fun qrCodeScanned(encodedData: String) {
+        val data = encodedData.split(';', ignoreCase = false, limit = 3)
+        if (data.size < 3) {
+            Timber.e("malformed data: %s", data)
+            return
+        }
+        val keyEncoded = data[2]
+        val keyBinary = Base64.decode(keyEncoded, Base64.DEFAULT)
+        val key = CleartextKeysetHandle.read(BinaryKeysetReader.withBytes(keyBinary))
+        Timber.d("key info: %s", key.keysetInfo)
     }
 }
